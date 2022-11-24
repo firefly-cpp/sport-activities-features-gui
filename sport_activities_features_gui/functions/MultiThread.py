21b3ca2893a152f440663e2bc509d615b18030f0
Nejc Graj <nejcgraj@gmail.com>
2022-11-24 19:15:56 +0100

    fixed bulkload
    fixed an issue with mapping arguments within the process pool

--- sport_activities_features_gui/functions/MultiThread.py
+++ sport_activities_features_gui/functions/MultiThread.py
@@ -1,6 +1,6 @@
 from sport_activities_features.tcx_manipulation import TCXFile
 import pandas as pd
-
+from itertools import product
 from multiprocessing import Pool, Manager
 
 class MultiThread:
@@ -14,21 +14,20 @@ class MultiThread:
             integral_metrics = tcx_file.extract_integral_metrics(path_of_file)
             all_data= activity | integral_metrics
             df = pd.DataFrame(all_data)
-            
             return df
         except:
             return pd.DataFrame
 
-    def _single_load_inner(path_of_file: str, arr: list, count: int): 
+    def _single_load_inner(self, path_of_file: str, data): 
         try:
             tcx_file = TCXFile()
             activity = tcx_file.read_one_file(path_of_file)
             integral_metrics = tcx_file.extract_integral_metrics(path_of_file)
             all_data= activity | integral_metrics
-            arr.append(all_data)
+            data.append(all_data)
         except:
+            a=1
             # better error handling
-            count+=1
     
     def bulk_load(self, directory_path: str, num_of_processes: int) -> dict:
         tcx_file = None
@@ -41,13 +40,12 @@ class MultiThread:
             return None
         with Manager() as manager:
             data = manager.list()
-            count = manager.Value('i')
             with Pool(processes=num_of_processes) as pool:
-                # this might work i think 
-                pool.starmap(self._single_load_inner, [(file, data, count) for file in files])
-            # print(data)
+                pool.starmap(self._single_load_inner, [(file, data) for file in files])
+                # pool.map(self._single_load_inner, product(files, data, arrError))
+                
             return {
-                'data' : pd.DataFrame(data),
+                'data' : pd.DataFrame(list(data)),
                 'numOfFiles': len(files),
-                'numOfFilesNotRead' : count 
+                'numOfFilesNotRead' : (len(files) - len(data))
             }
--- sport_activities_features_gui/models/User.py
+++ sport_activities_features_gui/models/User.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import json
 class User:
-    def __init__(self, _username: str, _data: pd.DataFrame, _pathToData: str, _setting: dict):
-        username = _username
-        data = _data
-        pathToData =_pathToData
-        setting = _setting
+    def __init__(self, _username: str, _data: pd.DataFrame, _userPath: str, _setting: dict):
+        self.username = _username
+        self.data = _data
+        self.userPath =_userPath
+        self.setting = _setting
+    def saveData(self):
+        try:
+            jsonStr = json.dumps(__dict__)
+            
+        except:
+            return 'something went wrong while saving'
+    

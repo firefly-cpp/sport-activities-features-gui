dee2ee4f37f858e30d216e3db1da639b136eb856
firefly-cpp <iztok@iztok-jr-fister.eu>
2023-07-10 11:29:43 +0200

    fix imports (TODO)

--- sport_activities_features_gui/__init__.py
+++ sport_activities_features_gui/__init__.py
@@ -1,1 +1,13 @@
+from sport_activities_features_gui import widgets
+from sport_activities_features_gui import windows
+from sport_activities_features_gui import models
+from sport_activities_features_gui import logic
+
+__all__ = [
+    'widgets',
+    'windows',
+    'models',
+    'logic'
+]
+
 __version__ = '0.2.0'

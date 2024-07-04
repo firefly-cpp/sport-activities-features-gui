9684e2da095d396e4473bb165109aaacc3737c8c
Zala Lahovnik <zala.lahovnik@gmail.com>
2024-07-04 12:04:29 +0200

    feat: add stylesheet, styles to application

--- sport_activities_features_gui/main.py
+++ sport_activities_features_gui/main.py
@@ -1,11 +1,17 @@
 import sys
 from PyQt6 import QtWidgets
 from sport_activities_features_gui.windows.profiles_window import Ui_ProfilesWindow
+import os
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle('Fusion')
+
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'styles.qss'), 'r') as f:
+        style = f.read()
+        app.setStyleSheet(style)
+
     profilesWindow = Ui_ProfilesWindow()
     profilesWindow.show()
     sys.exit(app.exec())

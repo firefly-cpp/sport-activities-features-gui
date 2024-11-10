71f8603c8d5cd7e521c2bf7b73aac4a57860f76b
Iztok Fister Jr. <iztok@iztok.xyz>
2024-11-10 14:54:35 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'sport-activities-features-gui'
 copyright = '2023-2024, TODO'
 author = 'TODO'
-release = '0.3.1'
+release = '0.3.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- sport_activities_features_gui/__init__.py
+++ sport_activities_features_gui/__init__.py
@@ -10,4 +10,4 @@ __all__ = [
     'logic'
 ]
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'

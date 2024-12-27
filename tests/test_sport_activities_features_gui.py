9ddecaf36d09fd99948750c77e89eab5e716de79
Miha Mirt <mihamirt27@gmail.com>
2024-12-27 12:21:50 +0100

    Add class to test

--- tests/test_sport_activities_features_gui.py
+++ tests/test_sport_activities_features_gui.py
@@ -1,5 +1,6 @@
 from sport_activities_features_gui import __version__
+from unittest import TestCase
 
-
-def test_version():
-    assert __version__ == '0.3.2'
+class TestSportActivitiesFeaturesGui(TestCase):
+    def test_version():
+        assert __version__ == '0.3.2'

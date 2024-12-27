255fbad7d92b8cd51423b98bfc9a52dbe94c6d6d
Miha Mirt <mihamirt27@gmail.com>
2024-12-27 12:29:28 +0100

    Fix Test

--- tests/test_sport_activities_features_gui.py
+++ tests/test_sport_activities_features_gui.py
@@ -2,5 +2,5 @@ from sport_activities_features_gui import __version__
 from unittest import TestCase
 
 class TestSportActivitiesFeaturesGui(TestCase):
-    def test_version():
+    def test_version(self):
         assert __version__ == '0.3.2'
